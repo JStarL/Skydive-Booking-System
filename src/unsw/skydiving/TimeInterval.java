0a0695a316262f1e3909a6a443cb4ce6c5ac9617
Jibitesh Saha <z5280740@student.unsw.edu.au>
2020-10-14 18:44:36 +1100

    Cleaned up TimeInterval

--- src/unsw/skydiving/TimeInterval.java
+++ src/unsw/skydiving/TimeInterval.java
@@ -5,30 +5,48 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class TimeInterval implements Comparable<TimeInterval> {
+    
+    /**
+     * The LocalDate of the interval
+     */
     private LocalDate date;
+
+    /**
+     * The starting LocalTime of the interval
+     */
     private LocalTime start;
+    /**
+     * The ending LocalTime of the interval
+     */
     private LocalTime end;
 
+    /**
+     * Standard Constructor for TimeInterval
+     * @param date A LocalDate object specifying the date
+     * @param start A LocalTime object specifying the date
+     * @param end A LocalTime object specifying the date
+     */
     public TimeInterval(LocalDate date, LocalTime start, LocalTime end) {
         this.date = date;
         this.start = start;
         this.end = end;
     }
 
+    /**
+     * Special constructor, initialising a 'moment in time' as a TimeInterval
+     * @param dateTime A LocalDateTime object specifying the moment in time
+     */
     public TimeInterval(LocalDateTime dateTime) {
         this.date = dateTime.toLocalDate();
         this.start = dateTime.toLocalTime();
         this.end = dateTime.toLocalTime();
     }
 
-    public boolean timeLiesIn(LocalTime time) {
-        return start.compareTo(time) <= 0 && end.compareTo(time) >= 0;
-    }
-
-    public int compareDate(LocalDate date) {
-        return this.date.compareTo(date);
-    }
-
+    /**
+     * Checks whether this interval overlaps with the given TimeInterval
+     * @param interval TimeInterval object to be checked against
+     * @return true if intervals overlap, false otherwise
+     */
     public boolean isOverlapping(TimeInterval interval) {
         if (interval == null) {
             return false;
@@ -42,6 +60,12 @@ public class TimeInterval implements Comparable<TimeInterval> {
         return this.start.isBefore(interval.end) && interval.start.isBefore(this.end);
     }
 
+    /**
+     * Checks whether this interval contians the given interval.
+     * this contains t if t lies within the boundaries of this
+     * @param t Target TimeInterval to be checked with
+     * @return true if this contains t, false otherwise
+     */
     public boolean contains(TimeInterval t) {
         if (t == null) {
             return false;
@@ -54,6 +78,12 @@ public class TimeInterval implements Comparable<TimeInterval> {
         return this.start.compareTo(t.start) <= 0 && this.end.compareTo(t.end) >= 0;
     }
 
+    /**
+     * Comparison is made with the date and startTime,
+     * i.e. if two intervals on the same date start at the
+     * same time, regardless of when they end, they are equivalent
+     * for sorting purposes
+     */
     @Override
     public int compareTo(TimeInterval t) {
 
@@ -67,16 +97,31 @@ public class TimeInterval implements Comparable<TimeInterval> {
             return compareToValue;
         }
 
-        // Assume Stable Sort
-
-        // compareToValue = end.compareTo(t.end);
-        // if (compareToValue != 0) {
-        // return compareToValue;
-        // }
+        // Collections.sort() is a Stable Sort
+        // If two flight times have t1.compareTo(t2) = 0
+        // Then the flight which was registered first
+        // will remain before
 
         return 0;
     }
 
+    /**
+     * Compare this TimeInterval's date with the given date
+     * aacording to the compareTo() definition
+     * @param date to be compared with
+     * @return integer: negative = current is less than,
+     * positive = current is more than,
+     * zero = current is same as
+     */
+    public int compareDate(LocalDate date) {
+        return this.date.compareTo(date);
+    }
+
+    /**
+     * Adjusts the start time of this TimeInterval by minutes,
+     * minutes can be positive, negative, or zero.
+     * @param minutes integer to alter startTime
+     */
     public void adjustStartTime(int minutes) {
         if (minutes > 0) {
             start = start.plusMinutes(minutes);
@@ -86,6 +131,11 @@ public class TimeInterval implements Comparable<TimeInterval> {
         }
     }
 
+    /**
+     * Adjusts the end time of this TimeInterval by minutes,
+     * minutes can be positive, negative, or zero.
+     * @param minutes integer to alter endTime
+     */
     public void adjustEndTime(int minutes) {
         if (minutes > 0) {
             end = end.plusMinutes(minutes);
@@ -95,6 +145,10 @@ public class TimeInterval implements Comparable<TimeInterval> {
         }
     }
 
+    /**
+     * Returns a new copy of this TimeInterval
+     * @return TimeInterval clone of this
+     */
     public TimeInterval clone() {
         return new TimeInterval(date, start, end);
     }
@@ -119,6 +173,10 @@ public class TimeInterval implements Comparable<TimeInterval> {
 
     }
 
+    /**
+     * A String representation of a TimeInterval.
+     * Easy for debugging prints :)
+     */
     @Override
     public String toString() {
         return "date: " + date.toString() +
